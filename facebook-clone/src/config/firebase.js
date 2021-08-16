6ca125c808d9a6a68af3718dd1ed0c14a6d61edd
mohamad <msiysinyuy@gmail.com>
2021-08-16 14:02:35 +0100

    the facebook clone header is set for now.

---
+++ facebook-clone/src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiunj5vqeoJwK7uLOidPNXEF5RrqilKbA",
+  authDomain: "facebook-clone-a97e2.firebaseapp.com",
+  projectId: "facebook-clone-a97e2",
+  storageBucket: "facebook-clone-a97e2.appspot.com",
+  messagingSenderId: "197296547637",
+  appId: "1:197296547637:web:d18a2a804d6b7457dbce7c",
+  measurementId: "G-J3MHZT109N"
+};
